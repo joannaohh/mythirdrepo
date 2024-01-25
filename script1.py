569693446c173d47cdfb7cac95dd28d329ca1f28
joanna_oh1@baylor.edu <joanna_oh1@baylor.edu>
2024-01-25 12:56:18 -0600

    solved problems

--- script1.py
+++ script1.py
@@ -1,29 +1,29 @@
 #### PROBLEM 1 ####
 # Correc the mistakes and produce the following output:
 # Johnny, the total of your purchase is $1,688.70
+from plotly import offline
+from plotly.graph_objs import Scattergeo, Layout
 
-def calcTotal(amt,tax,name):
-    mytotal = amt * (1+tax)
-    print(f"{myname}, the total of your purchase is ${total:,.2f}")
+
+def calcTotal(amt, tax, name):
+    mytotal = price * (1+thetax)
+    print(f"{myname}, the total of your purchase is ${mytotal:,.2f}")
+
 
 myname = 'Johnny'
-price = 1,560
-thetax = 8.25%
+price = 1560
+thetax = 0.0825
 
-calcTotal(thetax,price,name)
-
-
-
+calcTotal(thetax, price, myname)
 
 
 ####  PROBLEM 2  ####
 # print out ONLY the integers in the list below #
-list1 = [1,'two',3.2,'four',5]
+list1 = [1, 'two', 3.2, 'four', 5]
 
 for i in list1:
-    print(i)
-
-
+    if type(i) == int:
+        print(i)
 
 
 #### PROBLEM 3 ####
@@ -32,25 +32,24 @@ for i in list1:
 # the filename is Baylor.html and it should
 # open up in your browser. The library is Plotly.
 
-from plotly.graph_objs import Scattergeo, Layout
-from plotly import offline
 
 lons = [-97.121041]
 lats = [31.546872]
-hover_texts = ['Baylor University<br>Enrollemt: 19,297<br>Male: 7,718<br>Female: 11,579']
+hover_texts = [
+    'Baylor University<br>Enrollemt: 19,297<br>Male: 7,718<br>Female: 11,579']
 
 
 data = [{
     'type': 'scattergeo',
     'lon': lons,
     'lat': lats,
-    'hovertext':hover_texts,
-    'hoverinfo':"text",
-    'marker': {'size':10,}
-    }]
+    'hovertext': hover_texts,
+    'hoverinfo': "text",
+    'marker': {'size': 10, }
+}]
 
-my_layout = Layout(title='Baylor University',geo_scope='usa')
+my_layout = Layout(title='Baylor University', geo_scope='usa')
 
-fig = {'data':data, 'layout':my_layout}
+fig = {'data': data, 'layout': my_layout}
 
-offline.plot(fig,filename='Baylor.html')
+offline.plot(fig, filename='Baylor.html')
